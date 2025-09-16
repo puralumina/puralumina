4b5b729a27f7f8fc901f04522000edbb048eec6f
puralumina <puraluminaofficial@gmail.com>
2025-09-16 09:03:20 -0400

    Updated ShopPage.tsx

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -176,7 +176,7 @@ const ShopPage: React.FC = () => {
         <div className="max-w-7xl mx-auto px-4 py-4">
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-4">
-              <Link to="/" className="flex items-center text-gray-600 hover:text-gray-900">
+              <Link to="/shop" className="flex items-center text-gray-600 hover:text-gray-900">
                 <ArrowLeft className="w-5 h-5 mr-2" />
                 Back to Home
               </Link>
