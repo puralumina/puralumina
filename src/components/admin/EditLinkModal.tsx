45d7609358517f18d582b6f92d93d54437b5b871
puralumina <puraluminaofficial@gmail.com>
2025-10-03 02:50:24 -0400

    Updated EditLinkModal.tsx

--- src/components/admin/EditLinkModal.tsx
+++ src/components/admin/EditLinkModal.tsx
@@ -621,6 +621,7 @@ const EditLinkModal: React.FC<EditLinkModalProps> = ({ isOpen, onClose, link, on
                   className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 >
                   <option value="16:9">16:9 (Widescreen)</option>
+                  <option value="16:9">16:9 (Widescreen)</option>
                   <option value="4:3">4:3 (Standard)</option>
                   <option value="1:1">1:1 (Square)</option>
                   <option value="3:4">3:4 (Portrait)</option>
